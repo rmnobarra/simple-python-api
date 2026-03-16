5bf3819a98dff1383a22f0e519d5c68ec5c67f47
Leonardo Santos <rmnobarra@gmail.com>
2026-03-16 08:58:53 -0300

    feature: remove az deploy

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def delete_user(user_id):
     return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
